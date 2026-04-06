6813d060c40382852ec9214c26f9e74f0560b334
Osvin <ogamezm@miumg.edu.gt>
2026-04-06 07:31:53 -0600

    Tema Basics of OOP completado

---
+++ src/roadmap/Basics_of_OOP.java
@@ -0,0 +1,72 @@
+package roadmap;
+
+public class Basics_of_OOP {
+
+	public static void main(String[] args) {
+		
+	// Definicion de una clase
+		//public class MyClass {
+		//}
+		
+		
+	// Clase con campos
+		//public class Car {
+		
+		//    public String brand = null;
+		//    public String model = null;
+		//    public String color = null;
+
+		//}
+		
+	
+	// Clases con constructor
+		//public class Car {
+
+		 //   public String brand = null;
+		 //   public String model = null;
+		 //   public String color = null;
+
+		 //   public Car() {
+		  //  }
+
+		   // public Car(String theBrand, String theModel, String theColor) {
+		   //    this.brand = theBrand;
+		    //    this.model = theModel;
+		   //     this.color = theColor;
+		   // }
+		//}
+		
+		
+		// Clases con Metodos
+		//public class Car {
+
+		 //   public String brand = null;
+		   // public String model = null;
+		   // public String color = null;
+
+		  //  public void setColor(String newColor) {
+		   //     this.color = newColor;
+		   // }
+	//	}
+		
+		
+		// Clase con clase anidada
+		//public class MyClass {
+
+		  //  public static class MyNestedClass{
+		    //}
+		//}
+		
+
+		// Clases y objetos
+		//Car car1 = new Car();
+		//Car car2 = new Car();
+		//Car car3 = new Car();
+
+		//car1.setColor("red");
+		//car2.setColor("green");
+		//car3.setColor("blue");
+		
+	}
+
+}
